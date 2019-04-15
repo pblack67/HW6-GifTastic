c5ad80a38c35f755a637f73b41213cb4585fd7cc
Paul Black <pblack67@comcast.net>
2019-04-15 10:52:42 -0500

    Initial version, shows gifs

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # HW6-GifTastic
+
+This application accepts user input and queries the Giphy API and displays 10 GIFs that the API
+returns. 
+
+Giphy API Key = AtGUXnRVIUl0BcpMsuJfGwW6O7jLnt2G
---
+++ assets/javascript/gif.js
@@ -0,0 +1,41 @@
+var subjects = ["lions", "tigers", "bears"];
+
+var giphyAPI = "https://api.giphy.com/v1/gifs/search?api_key=AtGUXnRVIUl0BcpMsuJfGwW6O7jLnt2G&limit=10&q="
+
+function subjectButtonClicked(event) {
+    console.log(this);
+    $("#gifs").empty();
+    var giphyURL = giphyAPI + $(this).attr("data-name");
+    $.get(giphyURL).then(function(response) {
+        var data = response.data;
+        for (var i = 0; i < data.length; i++) {
+            console.log(data[i].images.fixed_height.url);
+            var image = $("<img>").attr("src", data[i].images.fixed_height.url);
+            $("#gifs").append(image);
+        }
+    });
+}
+
+function createButtons() {
+    $("#buttonList").empty();
+    for (var i = 0; i < subjects.length; i++) {
+        var newButton = $("<button>").
+            text(subjects[i]).
+            addClass("btn btn-primary subjectButton").
+            attr("data-name", subjects[i]);
+        $("#buttonList").append(newButton);
+    }
+    $(".subjectButton").on("click", subjectButtonClicked);
+}
+
+function addButtonClicked(event) {
+    console.log("addButtonClicked");
+    event.preventDefault();
+}
+
+// When dom is ready 
+$(function () {
+    $("#addButton").on("click", addButtonClicked);
+
+    createButtons();
+});

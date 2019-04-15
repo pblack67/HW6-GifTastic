c41ed56637fa63befc870c1aa8bf24704d0ebed1
Paul Black <pblack67@comcast.net>
2019-04-15 12:56:11 -0500

    Added image stop/start, clear functions

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,17 +1,31 @@
-var subjects = ["lions", "tigers", "bears"];
+var subjects = ["lions", "tigers", "bears", "dogs", "cats", "hamsters", "gerbils", "squirrels", "wombats", "kangaroos"];
 
 var giphyAPI = "https://api.giphy.com/v1/gifs/search?api_key=AtGUXnRVIUl0BcpMsuJfGwW6O7jLnt2G&limit=10&q="
 
+function imageClicked() {
+    if ($(this).attr("data-state") === "still") {
+        $(this).attr("data-state", "animated");
+        $(this).attr("src", $(this).attr("data-animated"));
+    } else {
+        $(this).attr("data-state", "still");
+        $(this).attr("src", $(this).attr("data-still"));
+    }
+}
+
 function subjectButtonClicked(event) {
-    console.log(this);
-    $("#gifs").empty();
     var giphyURL = giphyAPI + $(this).attr("data-name");
     $.get(giphyURL).then(function (response) {
+        console.log(response);
         var data = response.data;
         for (var i = 0; i < data.length; i++) {
-            console.log(data[i].images.fixed_height.url);
-            var image = $("<img>").attr("src", data[i].images.fixed_height.url);
-            $("#gifs").append(image);
+            var animatedURL = data[i].images.fixed_height.url;
+            var stillURL = data[i].images.fixed_height_still.url;
+            var stillImage = $("<img>").attr("src", stillURL).
+                attr("data-still", stillURL).
+                attr("data-animated", animatedURL).
+                attr("data-state", "still");
+            $("#gifs").prepend(stillImage);
+            stillImage.on("click", imageClicked);
         }
     });
 }
@@ -21,7 +35,7 @@ function createButtons() {
     for (var i = 0; i < subjects.length; i++) {
         var newButton = $("<button>").
             text(subjects[i]).
-            addClass("btn btn-primary subjectButton").
+            addClass("btn btn-primary subjectButton m-2").
             attr("data-name", subjects[i]);
         $("#buttonList").append(newButton);
     }
@@ -29,18 +43,26 @@ function createButtons() {
 }
 
 function addButtonClicked(event) {
-    console.log("addButtonClicked");
     event.preventDefault();
     var buttonName = $("#subjectName").val();
-    if (subjects.indexOf(buttonName) === -1) {
-        subjects.push(buttonName);
+    if (buttonName !== "") {
+        if (subjects.indexOf(buttonName) === -1) {
+            subjects.push(buttonName);
+        }
     }
     createButtons();
+    $("#subjectName").val("");
+}
+
+function clearButtonClicked() {
+    $("#gifs").empty();
 }
 
 // When dom is ready 
 $(function () {
     $("#addButton").on("click", addButtonClicked);
+
+    $("#clearButton").on("click", clearButtonClicked);
 
     createButtons();
 });
